9bca4d1c3ea8dd0975b664dd51066aef46f6bdd7
Sea <s1511536@s.tsukuba.ac.jp>
2017-05-15 10:21:14 +0900

    deploy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
-  # GET /users
-  # GET /users.json
+  #  GET /users
+  #  GET /users.json
   def index
     @users = User.all
   end
